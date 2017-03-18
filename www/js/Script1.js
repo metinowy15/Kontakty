76ffe0a70e17558d4175100a7107f9ec89254fdd
Jakub koza <Jakub koza>
2017-03-18 22:56:36 +0100

    poprawka email

--- www/js/Script1.js
+++ www/js/Script1.js
@@ -74,8 +74,8 @@ function dodajKontakt() {
        // var emails = [];
        // emails[0] = new ContactField(Email.value);
        // contact.emails = emails;
-        var addresses = [];
-        addresses = new ConctactField("home", Adres.value, true);
+        //var addresses = [];
+        //addresses = new ConctactField("home", Adres.value, true);
         contact.save(onSuccess, onError);
 
 

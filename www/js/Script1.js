6fd5c40eee73282451b3bbf1e2aac9f104fc2e60
Jakub koza <Jakub koza>
2017-03-18 22:36:55 +0100

    polepszenie wygladu

--- www/js/Script1.js
+++ www/js/Script1.js
@@ -3,15 +3,31 @@ window.addEventListener("DOMContentLoaded",startStrony);
 
 let Telefon;
 let Imie;
+let Nazwisko;
+let Email;
+let Zdjecie;
+let Nick;
+let Adres;
 let Poprawnosc;
+let Ukryty;
+let Rozszerzenie;
+let czyWiecej = false;
 
 function startStrony(){
-
+    //pola z danymi
     Telefon=document.getElementById("Telefon");
     Imie = document.getElementById("Imie");
+    Nazwisko = document.getElementById("Nazwisko");
+    Email = document.getElementById("Email");
+    Zdjecie = document.getElementById("Zdjecie");
+    Nick = document.getElementById("Nick");
+    Adres = document.getElementById("Adres");
+    //mechanizm skryptu
+    Ukryty = document.getElementById("Ukryty");
     Poprawnosc = document.getElementById("Poprawnosc");
+    Rozszerzenie = document.getElementById("Rozszerzenie");
     window.addEventListener("deviceready", start);
-
+    Rozszerzenie.addEventListener("click", aktywacja);
 
 
 }
@@ -26,26 +42,60 @@ function dodajKontakt() {
 
     function onSuccess() {
 
-        Poprawnosc.innerHTML = "Dodano kontakt: " + contact.displayName;
+        navigator.notification.alert(Imie.value + " dodany do kontaktów.", function () { }, "Zrobione!", "ok");
+        Imie.value = "";
+        Nazwisko.value = "";
+        Telefon.value = "";
     };
 
     function onError() {
 
-        Poprawnosc.innerHTML = "Nie uda³o sie dodaæ b³¹d: " + contactError.code
+        navigator.notification.alert("Wyst¹pi³ b³¹d", function () { }, "B³¹d :(", "ok");
     };
 
 
+
+
+
     var contact = navigator.contacts.create();
-    contact.displayName = Imie.value;
-    contact.nickname = Imie.value;
 
     var name = new ContactName();
-    name.givenName = "Kuba";
-    name.familyName = "Zelek";
+    name.givenName = Imie.value;
+    name.familyName = Nazwisko.value;
     contact.name = name;
+    var phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('mobile', Telefon.value, true);
+    contact.phoneNumbers = phoneNumbers;
+    if (czyWiecej == true) {
+        
 
-    contact.save(onSuccess, onError);
+        contact.nickname = Nick.value;
+        var emails = [];
+        emails[0] = new ContactField(Email.value);
+        contact.emails = emails;
+        var addresses = [];
+        addresses = new ConctactField("home", Adres.value, true);
+        contact.save(onSuccess, onError);
 
+
+    } else {
+        contact.save(onSuccess, onError);
+    }
+
+}
+function aktywacja() {
+
+    
+    var Ukryty = document.getElementById("Ukryty");
+    Ukryty.classList.toggle("aktywny");
+    if (Ukryty.className.includes("aktywny"[2])) {
+        czyWiecej = true;
+        Rozszerzenie.innerHTML = "Podstawowe opcje"
+    } else {
+        czyWiecej = false;
+        Rozszerzenie.innerHTML = "Dodatkowe opcje";
+    }
+    console.log(Ukryty.classList.item(2));
 
 }
 

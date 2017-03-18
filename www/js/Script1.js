209d59ae5f5173acb61ed8b8228edd4a943ef9c8
Jakub koza <Jakub koza>
2017-03-18 12:36:30 +0100

    Dodawanie kontaktu do telefonu

--- www/js/Script1.js
+++ www/js/Script1.js
@@ -1,1 +1,51 @@
 // JavaScript source code
+window.addEventListener("DOMContentLoaded",startStrony);
+
+let Telefon;
+let Imie;
+let Poprawnosc;
+
+function startStrony(){
+
+    Telefon=document.getElementById("Telefon");
+    Imie = document.getElementById("Imie");
+    Poprawnosc = document.getElementById("Poprawnosc");
+    window.addEventListener("deviceready", start);
+
+
+
+}
+function start(){
+
+
+
+
+
+}
+function dodajKontakt() {
+
+    function onSuccess() {
+
+        Poprawnosc.innerHTML = "Dodano kontakt: " + contact.displayName;
+    };
+
+    function onError() {
+
+        Poprawnosc.innerHTML = "Nie uda³o sie dodaæ b³¹d: " + contactError.code
+    };
+
+
+    var contact = navigator.contacts.create();
+    contact.displayName = Imie.value;
+    contact.nickname = Imie.value;
+
+    var name = new ContactName();
+    name.givenName = "Kuba";
+    name.familyName = "Zelek";
+    contact.name = name;
+
+    contact.save(onSuccess, onError);
+
+
+}
+
